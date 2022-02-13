c5e46f6ba96e65889f2ced142fd095ff0dea9818
Your Name <you@example.com>
2022-02-13 17:19:22 +0000

    add app theme

---
+++ constants/theme.ts
@@ -0,0 +1,72 @@
+import {Dimensions} from 'react-native';
+const {width,height} = Dimensions.get('window');
+
+
+export const COLORS = {
+  // base colors
+  primary: '#FC6D3F', // orange
+  secondary: '#CDCDD2', // gray
+
+  // colors
+  black: '#1E1F20',
+  white: '#FFFFFF',
+
+  lightGray: '#F5F5F6',
+  lightGray2: '#F6F6F7',
+  lightGray3: '#EFEFF1',
+  lightGray4: '#F8F8F9',
+  transparent: 'transparent',
+  darkgray: '#898C95',
+
+};
+
+export const SIZES = {
+  // global sizes
+  base: 8,
+  font: 14,
+  radius: 30,
+  padding: 10,
+  padding2: 12,
+
+  // font sizes
+  largeTitle: 50,
+  h1: 30,
+  h2: 22,
+  h3: 20,
+  h4: 18,
+  body1: 30,
+  body2: 20,
+  body3: 16,
+  body4: 14,
+  body5: 12,
+
+  // app dimensions
+  width,
+  height,
+
+    // line height
+    normal:22,
+    long:30,
+    extraLong:36
+};
+
+export const FONTS = {
+    largeTitle: {
+        fontFamily: 'Roboto-regular',
+        fontSize: SIZES.largeTitle,
+        lineHeight: 55,
+      },
+      h1: {fontFamily: 'Roboto-Black', fontSize: SIZES.h1, lineHeight: SIZES.extraLong},
+      h2: {fontFamily: 'Roboto-Bold', fontSize: SIZES.h2, lineHeight: SIZES.long},
+      h3: {fontFamily: 'Roboto-Bold', fontSize: SIZES.h3, lineHeight: SIZES.normal},
+      h4: {fontFamily: 'Roboto-Bold', fontSize: SIZES.h4, lineHeight: SIZES.normal},
+      body1: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body1, lineHeight: SIZES.extraLong},
+      body2: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body2, lineHeight: SIZES.long},
+      body3: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body3, lineHeight: SIZES.normal},
+      body4: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body4, lineHeight: SIZES.normal},
+      body5: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body5, lineHeight: SIZES.normal},
+};
+
+const appTheme = {COLORS,SIZES,FONTS};
+
+export default appTheme;
