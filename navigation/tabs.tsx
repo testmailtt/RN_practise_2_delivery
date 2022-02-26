528d1e1d5beed43cec5e6e4d47a6c47e5119b745
Your Name <you@example.com>
2022-02-26 19:05:42 +0000

    finish custom navigations

--- navigation/tabs.tsx
+++ navigation/tabs.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {View, Image, TouchableOpacity} from 'react-native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  createBottomTabNavigator,
+  BottomTabBar,
+} from '@react-navigation/bottom-tabs';
 import {Home} from '../screens';
 import {COLORS, icons} from '../constants';
+import Svg, {Path} from 'react-native-svg';
+import {isIphoneX} from 'react-native-iphone-x-helper';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,7 +17,7 @@ const Tab = createBottomTabNavigator();
  * @param {any} icon - The icon to be displayed.
  * @returns A function that returns a component.
  */
-const tabBarIcon = (focused:boolean, icon:any) => {
+const tabBarIcon = (focused: boolean, icon: any) => {
   return (
     <Image
       source={icon}
@@ -25,14 +30,94 @@ const tabBarIcon = (focused:boolean, icon:any) => {
     />
   );
 };
+const CustomTabBar = props => {
+  if (isIphoneX()) {
+    return (
+      <View>
+        <View
+          style={{
+            backgroundColor: COLORS.white,
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: 30,
+          }}>
+          <BottomTabBar {...props.props} />
+        </View>
+      </View>
+    );
+  } else {
+    return <BottomTabBar {...props.props} />;
+  }
+};
+const CustomTabBarButton = ({accessibilityState, children, onPress}) => {
+  let isSelected = accessibilityState.selected;
+  if (isSelected) {
+    return (
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={{flexDirection: 'row', position: 'absolute', top: 0}}>
+          <View style={{flex: 1, backgroundColor: COLORS.white}} />
+          <Svg width={70} height={61} viewBox="0 0 75 61">
+            <Path
+              d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+              fill={COLORS.white}
+            />
+          </Svg>
+          <View style={{flex: 1, backgroundColor: COLORS.white}} />
+        </View>
+        <TouchableOpacity
+          style={{
+            top: -22.5,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            backgroundColor: COLORS.white,
+          }}
+          onPress={onPress}>
+          {children}
+        </TouchableOpacity>
+      </View>
+    );
+  } else {
+    return (
+      <TouchableOpacity
+        style={{
+          flex: 1,
+          height: 60,
+          backgroundColor: COLORS.white,
+        }}
+        activeOpacity={1}
+        onPress={onPress}>
+        {children}
+      </TouchableOpacity>
+    );
+  }
+};
 const Tabs = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          backgroundColor: 'transparent',
+          elevation: 0,
+          borderTopWidth: 0,
+          position: 'absolute',
+          left: 0,
+          bottom: 0,
+          right: 0,
+        },
+      }}
+      tabBar={props => <CustomTabBar props={props} />}>
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
           tabBarIcon: ({focused}) => tabBarIcon(focused, icons.cutlery),
+          tabBarButton: props => <CustomTabBarButton {...props} />,
         }}
       />
       <Tab.Screen
@@ -40,6 +125,7 @@ const Tabs = () => {
         component={Home}
         options={{
           tabBarIcon: ({focused}) => tabBarIcon(focused, icons.search),
+          tabBarButton: props => <CustomTabBarButton {...props} />,
         }}
       />
       <Tab.Screen
@@ -47,6 +133,7 @@ const Tabs = () => {
         component={Home}
         options={{
           tabBarIcon: ({focused}) => tabBarIcon(focused, icons.like),
+          tabBarButton: props => <CustomTabBarButton {...props} />,
         }}
       />
       <Tab.Screen
@@ -54,6 +141,7 @@ const Tabs = () => {
         component={Home}
         options={{
           tabBarIcon: ({focused}) => tabBarIcon(focused, icons.user),
+          tabBarButton: props => <CustomTabBarButton {...props} />,
         }}
       />
     </Tab.Navigator>
