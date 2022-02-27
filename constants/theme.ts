6196f174a0ddfca17b36a58982a5e73f7f57c532
Your Name <you@example.com>
2022-02-27 19:45:32 +0000

    finish home screen header

--- constants/theme.ts
+++ constants/theme.ts
@@ -50,23 +50,35 @@ export const SIZES = {
     extraLong:36
 };
 
-export const FONTS = {
-    largeTitle: {
-        fontFamily: 'Roboto-regular',
-        fontSize: SIZES.largeTitle,
-        lineHeight: 55,
-      },
-      h1: {fontFamily: 'Roboto-Black', fontSize: SIZES.h1, lineHeight: SIZES.extraLong},
-      h2: {fontFamily: 'Roboto-Bold', fontSize: SIZES.h2, lineHeight: SIZES.long},
-      h3: {fontFamily: 'Roboto-Bold', fontSize: SIZES.h3, lineHeight: SIZES.normal},
-      h4: {fontFamily: 'Roboto-Bold', fontSize: SIZES.h4, lineHeight: SIZES.normal},
-      body1: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body1, lineHeight: SIZES.extraLong},
-      body2: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body2, lineHeight: SIZES.long},
-      body3: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body3, lineHeight: SIZES.normal},
-      body4: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body4, lineHeight: SIZES.normal},
-      body5: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body5, lineHeight: SIZES.normal},
-};
+// export const FONTS = {
+//     largeTitle: {
+//         fontFamily: 'Roboto-regular',
+//         fontSize: SIZES.largeTitle,
+//         lineHeight: 55,
+//       },
+//       h1: {fontFamily: 'Roboto-Black', fontSize: SIZES.h1, lineHeight: SIZES.extraLong},
+//       h2: {fontFamily: 'Roboto-Bold', fontSize: SIZES.h2, lineHeight: SIZES.long},
+//       h3: {fontFamily: 'Roboto-Bold', fontSize: SIZES.h3, lineHeight: SIZES.normal},
+//       h4: {fontFamily: 'Roboto-Bold', fontSize: SIZES.h4, lineHeight: SIZES.normal},
+//       body1: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body1, lineHeight: SIZES.extraLong},
+//       body2: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body2, lineHeight: SIZES.long},
+//       body3: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body3, lineHeight: SIZES.normal},
+//       body4: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body4, lineHeight: SIZES.normal},
+//       body5: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body5, lineHeight: SIZES.normal},
+// };
 
+export const FONTS = {
+  largeTitle: { fontFamily: "Roboto-regular", fontSize: SIZES.largeTitle, lineHeight: 55 },
+  h1: { fontFamily: "Roboto-Black", fontSize: SIZES.h1, lineHeight: 36 },
+  h2: { fontFamily: "Roboto-Bold", fontSize: SIZES.h2, lineHeight: 30 },
+  h3: { fontFamily: "Roboto-Bold", fontSize: SIZES.h3, lineHeight: 22 },
+  h4: { fontFamily: "Roboto-Bold", fontSize: SIZES.h4, lineHeight: 22 },
+  body1: { fontFamily: "Roboto-Regular", fontSize: SIZES.body1, lineHeight: 36 },
+  body2: { fontFamily: "Roboto-Regular", fontSize: SIZES.body2, lineHeight: 30 },
+  body3: { fontFamily: "Roboto-Regular", fontSize: SIZES.body3, lineHeight: 22 },
+  body4: { fontFamily: "Roboto-Regular", fontSize: SIZES.body4, lineHeight: 22 },
+  body5: { fontFamily: "Roboto-Regular", fontSize: SIZES.body5, lineHeight: 22 },
+};
 const appTheme = {COLORS,SIZES,FONTS};
 
 export default appTheme;
